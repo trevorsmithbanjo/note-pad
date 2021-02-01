37a1aed46096c70c7cde35a9a06668a9cf3ec4dd
Trevor Smith <trevorsmithbanjo@gmail.com>
2021-01-31 18:42:52 -0600

    Get Routes Working.

--- server.js
+++ server.js
@@ -8,3 +8,7 @@ const PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
+app.get('/api/notes', (req, res) => res.sendFile(path.join(__dirname, 'db/db.json')));
